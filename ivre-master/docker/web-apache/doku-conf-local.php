d8547706ad5e7833eff0c4137610719e0680d601
ZZ <zlatk0@outlook.com>
2016-06-14 01:59:18 +0200

    Revert "Scan"
    This reverts commit db7644c251d0bdaa4b52c399ddfa9996490913ce.

--- ivre-master/docker/web-apache/doku-conf-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conf['title'] = 'IVRE';
-$conf['lang'] = 'en';
-$conf['license'] = '0';
-$conf['useacl'] = 0;
-$conf['superuser'] = '@admin';
-$conf['disableactions'] = 'register';
-$conf['start'] = 'doc:readme';
-$conf['userewrite'] = 1;
--- ivre-master/docker/web/doku-conf-acl.auth.php
+++
@@ -1,7 +0,0 @@
-# acl.auth.php
-# <?php exit()?>
-# Don't modify the lines above
-#
-# Access Control Lists
-#
-*               @ALL          8
